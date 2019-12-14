4d44ac0c39e110e583100109a3c23d2f6db76468
Girik soni <giriksoni0710@gmail.com>
2019-12-14 13:56:02 -0800

    final working clock

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
+var HOURHAND = document.getElementById("hour");
+var MINUTEHAND = document.getElementById("minute");
+var SECONDHAND = document.getElementById("second");
 
-function getCurrentDate(){
-
+function getCurrentDateAndPosition(){
+    
     var hours = new Date().getHours()
     var minutes = new Date().getMinutes()
     var seconds = new Date().getSeconds()
 
-    console.log(hours)
-}
+    var degreeSeconds = (360/60)*parseInt(seconds)
+    var degreeminutes = (360/60)*parseInt(minutes)+(degreeSeconds/60)
+    var degreehours = (360/12)*parseInt(hours)+(degreeminutes/12)    
 
-getCurrentDate();
+    
+     
+    HOURHAND.style.transform = "rotate("+degreehours+"deg)";
+    MINUTEHAND.style.transform = "rotate("+degreeminutes+"deg)";
+    SECONDHAND.style.transform = "rotate("+degreeSeconds+"deg)";
+
+    }
+    
+setInterval(getCurrentDateAndPosition, 1000)
+
+
